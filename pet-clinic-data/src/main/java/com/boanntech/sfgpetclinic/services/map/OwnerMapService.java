809d244967b4b6e5fcce40f514a48296946ae9bf
Sean Lynch <sean.p.w.lynch@gmail.com>
2020-04-09 12:32:40 +0100

    Update profiles to use spring data jpa services closes #55

--- pet-clinic-data/src/main/java/com/boanntech/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/boanntech/sfgpetclinic/services/map/OwnerMapService.java
@@ -5,11 +5,13 @@ import com.boanntech.sfgpetclinic.model.Pet;
 import com.boanntech.sfgpetclinic.services.OwnerService;
 import com.boanntech.sfgpetclinic.services.PetService;
 import com.boanntech.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
